434d6c56a9d1ee45d7db30d898d63f08549f927c
Nika Kopadze <nikolozkopadze@gmail.com>
2023-05-13 23:45:09 +0400

    updated register page functionality

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,19 +1,57 @@
-import { Row, Card, Form, Space, Typography, Layout } from 'antd';
+import { Row, Card, Form, Space, Typography } from 'antd';
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { CustomButton, CustomInput } from '../components';
 import PasswordInput from '../components/CustomInputs/PasswordInput';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Paths } from '../Paths';
+import { useAppSelector } from '../app/hook';
+import { useRegisterMutation } from '../app/services/auth';
+import { isErrorWithMessage } from '../utils/errorMessage';
+import ErrorMsg from '../components/ErrorMsg';
+
+interface RegisterData {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
 
 const Register = () => {
+  const navigate = useNavigate();
+  const { user } = useAppSelector(state => state.auth);
+  const [register] = useRegisterMutation();
+  const [error, setError] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    if (user) {
+      navigate(Paths.home);
+    }
+  }, [user, navigate]);
+
+  const handleRegister = async (data: RegisterData) => {
+    try {
+      await register(data).unwrap();
+
+      navigate('/');
+    } catch (err) {
+      const maybeError = isErrorWithMessage(err);
+
+      if (maybeError) {
+        setError(err.data.message);
+      } else {
+        setError('Something went wrong');
+      }
+    }
+  };
+
   return (
     <Container>
       <Row align='middle' justify='center'>
         <Card title='Register' style={{ width: '300px' }}>
           <Form
-            onFinish={() => {
-              'login';
-            }}
+            name='register'
+            onFinish={handleRegister}
             style={{ marginBottom: '16px' }}
           >
             <CustomInput type='name' name='name' placeholder='First Name' />
@@ -38,6 +76,7 @@ const Register = () => {
             <Typography.Text>
               Already have an account? <Link to={Paths.login}>Login</Link>
             </Typography.Text>
+            <ErrorMsg message={error} />
           </Space>
         </Card>
       </Row>
